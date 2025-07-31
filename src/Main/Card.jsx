9b880738e3d7cf87c479f726f92dbd7911b4f3b6
islamabb <islamabbasov428@gmail.com>
2025-07-31 13:51:22 +0300

    Исправил код

--- src/Main/Card.jsx
+++ src/Main/Card.jsx
@@ -1,12 +1,12 @@
 import "./Card.scss"
 
-function Card() {
+function Card(props) {
   return(
     <div className="mainCard">
       <img className="card-photo"src="/img/main-photo.png" alt="photo"></img>
       <div className="Card">
-      <h2>Новости IT</h2>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam architecto incidunt in cupiditate vitae odit?</p>
+      <h2>{props.title}</h2>
+      <p>{props.desc}</p>
       <button className="btn-card">Подробнее</button>
       <button className="btn-like">
         <img src="/img/btn-like.png"></img>
--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -2,25 +2,60 @@ import "./Main.scss"
 import Card from "./Card"
 
 function Main (){
+
+  const arr = [
+    {
+      title: 'НОВОСТИ IT',
+      desc: 'Torem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. '
+    },
+    {
+      title: 'НОВОСТИ IT',
+      desc: 'Torem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. '
+    },
+    {
+      title: 'НОВОСТИ IT',
+      desc: 'Torem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. '
+    },
+    {
+      title: 'НОВОСТИ IT',
+      desc: 'Torem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. '
+    },
+    {
+      title: 'НОВОСТИ IT',
+      desc: 'Torem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. '
+    },
+    {
+      title: 'НОВОСТИ IT',
+      desc: 'Torem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. '
+    },
+    {
+      title: 'НОВОСТИ IT',
+      desc: 'Torem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. '
+    },
+    {
+      title: 'НОВОСТИ IT',
+      desc: 'Torem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. '
+    },
+    {
+      title: 'НОВОСТИ IT',
+      desc: 'Torem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. '
+    },
+
+  ];
+
   return(
-    <main className="main">
-      <h1 className="text"><b className="text1">Популярные</b> новости</h1>
-      <span className="main-span">
-      <Card/>
-      <Card/>
-      <Card/>
-      </span>
-      <span className="main-span">
-      <Card/>
-      <Card/>
-      <Card/>
-      </span>
-      <span className="main-span">
-      <Card/>
-      <Card/>
-      <Card/>
-      </span>
-    </main>
+    <div className="main">
+      <h1><span>Популярные</span> новости</h1>
+      <div className="container" >
+      {arr.map((item, index) => (<Card
+      key = {index}
+      title = {item.title}
+      desc =  {item.desc}
+      />)
+
+    )}
+      </div>
+    </div>
   )
 }
 
